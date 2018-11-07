f4e01e0f98736cc8187f0a50d6097a82d0530975
zdzulak <43354865+zdzulak@users.noreply.github.com>
2018-11-07 13:51:30 -0500

    first upload yay

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>About | COMP1002 HTML, CSS, and JS Fundamentals</title>
+		<meta name="author" content="The name of the document’s author to go here">
+		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body id="section6">
+		<!-- Page-level header -->
+		<header>
+			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<h1>HTML, CSS, and JS Fundamentals</h1>
+			<h2>Building Blocks for Web Developers</h2>
+<?php
+	require $_SERVER['DOCUMENT_ROOT'].'\includes\global-nav.php';
+?>
+		</header>
+		<!-- Page-level main content -->
+		<main>
+			<section>
+				<h3>About Us</h3>
+				<p>Turpis nunc eget lorem dolor sed. Vel facilisis volutpat est velit egestas dui id. Quam quisque id diam vel quam. Mauris a diam maecenas sed enim. Tortor posuere ac ut consequat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Vitae elementum curabitur vitae nunc sed velit. Mollis nunc sed id semper. Diam vel quam elementum pulvinar. Ultrices gravida dictum fusce ut.</p>
+
+				<p>Pellentesque sit amet porttitor eget. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Dictum fusce ut placerat orci nulla pellentesque dignissim. Rhoncus mattis rhoncus urna neque viverra. Id semper risus in hendrerit gravida rutrum quisque non tellus. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis.</p>
+
+				<p>Lorem mollis aliquam ut porttitor leo a diam. Elementum eu facilisis sed odio morbi quis commodo. Suspendisse interdum consectetur libero id faucibus nisl. Tempus urna et pharetra pharetra. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Sed faucibus turpis in eu mi bibendum. Sollicitudin aliquam ultrices sagittis orci.</p>
+			</section>
+		</main>
+		<!-- Page-level footer -->
+		<footer>
+<?php
+	require $_SERVER['DOCUMENT_ROOT'].'\includes\footer-nav.php';
+?>
+			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
+		</footer>
+	</body>
+</html>
